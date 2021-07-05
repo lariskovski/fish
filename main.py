f4c22450b5489ac1d9cc3998acdd3988df98d234
Larissa <37162993+lariskovski@users.noreply.github.com>
2021-07-05 10:48:24 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from random import choice
+
+phrase = ['woman', 'want', 'me', 'fish', 'fear', 'me']
+
+def scramble(phrase):
+    # output = ([choice(phrase) for i in range(len(phrase))])
+    output = []
+    for _ in range(len(phrase)):
+        word = choice(phrase)
+        output.append(word)
+        phrase.remove(word)
+    return output
+
+print(*scramble(phrase))
